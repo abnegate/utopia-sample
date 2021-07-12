e1d89f885327f27a82dfd8439f6117bd0337dd5c
Jake Barnby <jake@jakebarnby.com>
2021-07-13 00:41:00 +1200

    Get DB dsn from env

--- app/registry.php
+++ app/registry.php
@@ -1,8 +1,6 @@
 <?php
 
 use SampleAPI\Data\SimpleORM;
-use Swoole\Database\PDOConfig;
-use Swoole\Database\PDOPool;
 use Utopia\App;
 use Utopia\Cache\Adapter\None;
 use Utopia\Cache\Cache;
@@ -12,27 +10,15 @@ use Utopia\Registry\Registry;
 
 $registry = new Registry();
 
-$registry->set('dbPool', function () {
+$registry->set('orm', function () use ($registry) {
     $dbHost = App::getEnv('_APP_DB_HOST', 'localhost');
     $dbPort = App::getEnv('_APP_DB_PORT', '3306');
     $dbUser = App::getEnv('_APP_DB_USER', 'root');
     $dbPass = App::getEnv('_APP_DB_PASS', '');
-    $dbScheme = App::getEnv('_APP_DB_SCHEMA', 'test');
+    $dbScheme = App::getEnv('_APP_DB_SCHEMA', 'sample');
+    $dsn = "mysql:host=" . $dbHost . ";port=" . $dbPort . ";charset=utf8mb4";
 
-    return new PDOPool((new PDOConfig())
-        ->withHost($dbHost)
-        ->withPort($dbPort)
-        ->withDbName($dbScheme)
-        ->withUsername($dbUser)
-        ->withPassword($dbPass)
-    );
-});
-
-$registry->set('orm', function () use ($registry) {
-    $dbScheme = App::getEnv('_APP_DB_SCHEMA', 'test');
-
-    // To fix dependency cycle with dbPool
-    $pdo = new PDO("mysql:host=localhost;port=3306;charset=utf8mb4", 'root', '', [
+    $pdo = new PDO($dsn, $dbUser, $dbPass, [
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4',
         PDO::ATTR_TIMEOUT => 3, // Seconds
         PDO::ATTR_PERSISTENT => true,
